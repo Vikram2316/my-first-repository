11c74db405bd577d76bb3540cc0ed513cec2256e
Vikram Pai <vikramvpai426@gmail.com>
2025-01-26 19:28:10 -0500

    Task 2: Product Detail

--- Coding_Challenge_01/cc_1.js
+++ Coding_Challenge_01/cc_1.js
@@ -8,3 +8,13 @@ console.log("Employee Name:", employeeName, "Type:", typeof employeeName);
 onsole.log("Employee ID:", employeeID, "Type:", typeof employeeID);
 onsole.log("Is Active:", isActive, "Type:", typeof isActive);
 
+// Task 2: Primitive Data Types
+
+let productName = "Wilson NFL Football";
+const productPrice = 24.99;
+var isAvailable = True;
+
+console.log("Product Name:", productName, "Type:", typeof productName);
+console.log("Product Price:", productPrice, "Type:", typeof productPrice);
+console.log("Is Available:", isActive, "Type:", typeof isAvailable);
+
