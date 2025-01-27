64499cda62b09e0ea8055e11c80ed447fc7bb6b2
Vikram Pai <vikramvpai426@gmail.com>
2025-01-26 19:24:48 -0500

    Task 1: Employee Information

---
+++ Coding_Challenge_01/cc_1.js
@@ -0,0 +1,10 @@
+// Task 1: Variables 
+
+let employeename = "Tom Brady";
+const employeeID = 012;
+var isActive = true;
+
+console.log("Employee Name:", employeeName, "Type:", typeof employeeName);
+onsole.log("Employee ID:", employeeID, "Type:", typeof employeeID);
+onsole.log("Is Active:", isActive, "Type:", typeof isActive);
+
