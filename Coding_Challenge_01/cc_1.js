cc3a93213f14a4e1982155ed36c95f7e113c5006
Vikram Pai <vikramvpai426@gmail.com>
2025-01-26 19:30:08 -0500

    Task 3: Financial Transactions

--- Coding_Challenge_01/cc_1.js
+++ Coding_Challenge_01/cc_1.js
@@ -18,3 +18,22 @@ console.log("Product Name:", productName, "Type:", typeof productName);
 console.log("Product Price:", productPrice, "Type:", typeof productPrice);
 console.log("Is Available:", isActive, "Type:", typeof isAvailable);
 
+// Task 3: Number Data Type
+
+let accountBalance = 1000 
+console.log("Account Balance:", accountBalance);
+
+// Task 3.1: Arithmatic Operations
+
+accountBalance -= 400; //Withdraw 400
+console.log("Account Balance after withdrawing $400", accountBalance);
+
+accountBalance += 900; //Deposit 900
+console.log("Account Balance after depositing $900", accountBalance);
+
+accountBalance *= 1.03 // 3% Account Interest rate
+console.log("Account Balance after 3% interest rate", accountBalance);
+
+accountBalance /= 2 // Splitting money 2 ways 
+console.log("Account Balance after splitting money:", accountBalance);
+
